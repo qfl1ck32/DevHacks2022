f53daf9fd887f1458166c61bad047d2514b0facf
Rusu Andrei-Cristian <rusuandreicristian32@gmail.com>
2022-11-19 09:49:49 +0200

    add: web app boilerplate

---
+++ microservices/web/src/components/Loader/Loader.tsx
@@ -0,0 +1,3 @@
+export const Loader = () => {
+  return <div className="grid h-screen place-items-center animate-spin"></div>;
+};
---
+++ microservices/web/src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { Loader } from "@root/components/Loader/Loader";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <div className="grid h-screen place-items-center">Hi!</div>;
+};
+
+export default Home;
